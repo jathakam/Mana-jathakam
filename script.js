f78d9480c1059149e7dd2784ef0fbbfea0af27c1
jathakam <badra.king@gmail.com>
2026-07-16 23:06:11 +0530

    Fix string concatenation for request body in script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function generateJathakam() {
   const d = dob.split("-");
   const birthdate = `${d[2]}-${d[1]}-${d[0]}`;
 
-  const body = name=${encodeURIComponent(name)}&birthdate=${birthdate}&birthtime=${encodeURIComponent(time)}&City=${encodeURIComponent(place.toUpperCase())};
+  const body = `name=${encodeURIComponent(name)}&birthdate=${birthdate}&birthtime=${encodeURIComponent(time)}&City=${encodeURIComponent(place.toUpperCase())}`;
 
   try {
 
