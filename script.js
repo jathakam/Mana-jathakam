2975b9da04222cbca413e22fc16c9161fa9d4933
jathakam <badra.king@gmail.com>
2026-07-16 13:31:47 +0530

    Change X-RapidAPI-Key in request headers
    Updated the X-RapidAPI-Key in headers to a specific key.

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ async function generateJathakam() {
 
     headers:{
   "Content-Type":"application/x-www-form-urlencoded",
-  "X-RapidAPI-Key":"YOUR_API_KEY",
+  "X-RapidAPI-Key":"X-RapidAPI-Key": "a97e48a6b4msh1f863e6605b79e5p188a89jsnac6609305923
   "X-RapidAPI-Host":"kundli1.p.rapidapi.com"
 }
 
