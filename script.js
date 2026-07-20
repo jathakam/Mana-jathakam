d594baf34adc8fbe71fa69098d49b43ca5c29e90
jathakam <badra.king@gmail.com>
2026-07-20 13:04:29 +0530

    Remove API key from request headers
    Removed sensitive API key from headers.

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ async function generateJathakam() {
           "Content-Type":
             "application/x-www-form-urlencoded",
 
-          "X-RapidAPI-Key":
             "X-RapidAPI-Key": "a97e48a6b4msh1f863e6605b79e5p188a89jsnac6609305923",
 
           "X-RapidAPI-Host":
